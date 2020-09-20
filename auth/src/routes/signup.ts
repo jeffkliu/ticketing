f6ba71d0ef3c5c3e600362d19b6ec2b11e95b90c
Jeffrey Liu <jeffreliu@google.com>
2020-09-20 11:16:40 -0700

    up to error-checkin

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4-20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating a user...');
+
+    // new User({email,password})good
+    res.status(200).send({ email });
+  }
+);
+export { router as signUpRouter };
