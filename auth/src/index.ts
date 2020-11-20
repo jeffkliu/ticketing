5e150d88755d37a1eda340af48e94b80edc4a0ed
jeffkliu <jeffreyliu0@gmail.com>
2020-11-20 13:59:00 -0800

    added pub/sub NATS; updated @jlgittix/common package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
-import {app} from './app';
+import { app } from './app';
 
 const start = async () => {
-      // Generate JWT
-  if(!process.env.JWT_KEY){
-        throw new Error('Wrong secret');
-      }
+  // Generate JWT
+  if (!process.env.JWT_KEY) {
+    throw new Error('Wrong secret');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected to MongoDB');
+    console.log('Connected to Auth MongoDB');
   } catch (err) {
     console.error(err);
   }
