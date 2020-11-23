3a4a0b2c12e85001c0161d17ef36dbdc482e9781
jeffkliu <jeffreyliu0@gmail.com>
2020-11-22 18:23:49 -0800

    finished core handlers tests; added pub events & tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,47 @@
 import express, { Request, Response, Router } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@jlgittix/common';
+import { Order, OrderStatus } from '../models/order';
+import { NotBeforeError } from 'jsonwebtoken';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderid', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+
+    // publishing an event saying this was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+        price: order.ticket.price,
+      },
+    });
+
+    res.status(204).send(order);
+  },
+);
 
 export { router as deleteOrderRouter };
