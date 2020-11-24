7d572f30925299a4e5950bb7e7752ec6a88d1dbe
jeffkliu <jeffreyliu0@gmail.com>
2020-11-24 14:47:40 -0800

    added listener events

--- orders/rmdangledocker.sh
+++ orders/rmdangledocker.sh
@@ -3,4 +3,5 @@
 docker rmi $(docker images |grep 'jeffliu0/auth')
 docker rmi $(docker images |grep 'jeffliu0/tickets')
 docker rmi $(docker images |grep 'jeffliu0/client1')
+docker rmi $(docker images |grep 'jeffliu0/orders')
 docker rmi $(sudo docker images -f 'dangling=true' -q) -f
