77568790b70202cc56cb3260fc2ab8c660dcd410
Jeff Liu-personal <jeffreyliu0@gmail.com>
2020-11-28 12:19:28 -0800

    Updates

--- common/build/events/expiration-complete-event.d.ts
+++ common/build/events/expiration-complete-event.d.ts
@@ -1,9 +1,7 @@
 import { Subjects } from "./subjects";
-interface ExpirationCompleteEvent {
-  subject: Subjects.ExpirationComplete;
-  data: {
-    orderId: string;
-  };
+export interface ExpirationCompleteEvent {
+    subject: Subjects.ExpirationComplete;
+    data: {
+        orderId: string;
+    };
 }
-
-export { ExpirationCompleteEvent };
