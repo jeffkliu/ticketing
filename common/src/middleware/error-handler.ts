e4906f2ce885d938b54dcff77590b5344e06cce4
Jeff Liu-personal <jeffreyliu0@gmail.com>
2020-11-27 22:36:33 -0800

    Updates

---
+++ common/src/middleware/error-handler.ts
@@ -0,0 +1,23 @@
+import express, {
+  Request,
+  Response,
+  ErrorRequestHandler,
+  NextFunction,
+} from 'express';
+import { CustomError } from '../errors/custom-error';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  console.error(err);
+  return res.status(400).send({
+    errors: [{ message: 'Something went wrong lol' }],
+  });
+};
