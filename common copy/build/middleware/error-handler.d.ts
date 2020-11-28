cd20f1ec07180b46ff905a4b0d5fcf156926d9d5
Jeff Liu-personal <jeffreyliu0@gmail.com>
2020-11-27 21:55:45 -0800

    deleted common copy dir

--- common copy/build/middleware/error-handler.d.ts
+++
@@ -1,2 +0,0 @@
-import express, { Request, Response, NextFunction } from 'express';
-export declare const errorHandler: (err: Error, req: Request, res: Response, next: NextFunction) => express.Response<any>;
