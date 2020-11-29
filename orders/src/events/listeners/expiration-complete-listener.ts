4fb47f7a5ce2dce75630498dc008da53295a6fc1
Jeff Liu-personal <jeffreyliu0@gmail.com>
2020-11-28 17:59:41 -0800

    finally got all the events to work. something is up with the vsc caching. made some changes on tsconfig

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -1,14 +1,13 @@
 import {
-  ExpirationCompleteEvent,
   Listener,
   OrderStatus,
   Subjects,
+  ExpirationCompleteEvent,
 } from "@jlgittix/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Order } from "../../models/order";
 import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
-import { natsWrapper } from "../../nats-wrapper";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
@@ -23,7 +22,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({ status: OrderStatus.Cancelled });
     await order.save();
 
-    await new OrderCancelledPublisher(natsWrapper.client).publish({
+    await new OrderCancelledPublisher(this.client).publish({
       id: order.id,
       version: order.version,
       ticket: {
