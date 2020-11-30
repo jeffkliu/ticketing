ef037c87382d19618eab163a6c97d26d5cddcffb
Jeff Liu-personal <jeffreyliu0@gmail.com>
2020-11-29 19:18:30 -0800

    finally fixed the listener and it needed to be on orders service

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,31 @@
+import {
+  Subjects,
+  Listener,
+  PaymentCreatedEvent,
+  NotFoundError,
+  OrderStatus,
+} from "@jlgittix/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    order.set({
+      status: OrderStatus.Complete,
+    });
+
+    await order.save();
+
+    msg.ack();
+  }
+}
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -41,7 +41,6 @@ const start = async () => {
 
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
-    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
